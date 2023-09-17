c9727cef9977a3de80fbcc0d4c04b373408f8432
douglas321 <csper7227@byui.edu>
2023-09-16 19:46:36 -0600

    changed README

--- csharp-prep/Prep1/Program.cs
+++ csharp-prep/Prep1/Program.cs
@@ -5,5 +5,26 @@ class Program
     static void Main(string[] args)
     {
         Console.WriteLine("Hello Prep1 World!");
+
+        //string Interpolation
+        int  myInt = 5;
+        Console.WriteLine(myInt);
+        Console.WriteLine("my int =" + myInt);
+        Console.WriteLine($"my int = {myInt}");
+
+        //ReadLine
+        Console.Write("What is your name?");
+        string name = Console.ReadLine();
+        Console.WriteLine(name);
+
+        //Conditionals
+        if (name == "Carter")
+        {
+            Console.WriteLine("Welcome Carter!");
+        } 
+        else 
+        {
+            Console.WriteLine("Intruder!");
+        }
     }
 }
