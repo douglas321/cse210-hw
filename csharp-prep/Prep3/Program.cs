5eb92eea5adee1c4726f903607f0b1aa62ae1d7d
douglas321 <csper7227@byui.edu>
2023-09-20 15:26:28 -0600

    Random Number Guessing Game

--- csharp-prep/Prep3/Program.cs
+++ csharp-prep/Prep3/Program.cs
@@ -4,6 +4,36 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello Prep3 World!");
+        //get magic number
+
+        Random randomGenerator = new Random();
+        int magicNumber = randomGenerator.Next(1, 101);
+        
+
+        //game loop
+        bool keepPlaying = true;
+        while (keepPlaying == true)
+        {
+            //get guess
+            Console.WriteLine("What is your guess?");
+            string userInput = Console.ReadLine();
+            int userGuess = int.Parse(userInput);
+
+            //see if guess is correct
+            if (userGuess > magicNumber)
+            {
+                Console.WriteLine("Lower");
+            }
+            else if (userGuess < magicNumber)
+            {
+                Console.WriteLine("Higher");
+            }
+            else
+            {
+                Console.WriteLine("You got it!!");
+                keepPlaying = false;
+            }
+        }
+        
     }
 }
